ffe89b29aa0b4f072403472d50ddabee2ba6f38a
WAWOEGH <benjamin.wasielewski@awoberlin.de>
2025-04-03 10:19:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-document.getElementById("contact-form").addEventListener("submit", function(event) {
-    event.preventDefault();
-    document.getElementById("form-response").textContent = "Danke! Ihre Nachricht wurde gesendet.";
+document.addEventListener("DOMContentLoaded", () => {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const nav = document.querySelector(".nav");
+
+    menuToggle.addEventListener("click", () => {
+        nav.classList.toggle("active");
+    });
+
+    document.getElementById("contact-form").addEventListener("submit", function(event) {
+        event.preventDefault();
+        document.getElementById("form-response").textContent = "Danke! Ihre Nachricht wurde gesendet.";
+    });
 });
